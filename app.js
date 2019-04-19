efbbc6a17935ea5f7f6f343e7a2e626e8dd19577
quentin roux quentin.roux@hotmail.fr <quentin.roux@hotmail.fr>
2019-04-19 17:33:33 +0200

    shop with temp db

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-const http = require('http');
-const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
-const adminRoutes = require('./routes/admin.js');
-const shopRoutes = require('./routes/shop.js');
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'));
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
